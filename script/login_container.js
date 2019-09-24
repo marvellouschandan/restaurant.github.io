7483537436eb8aceaaee71e56cd3566bdc659554
Rajputakshay <akshaykumar772@yahoo.com>
2019-09-24 10:06:23 +0530

    added signup_container ,fixed footer and services

--- script/login_container.js
+++ script/login_container.js
@@ -6,3 +6,12 @@ document.getElementById('login-container').style.display = "inherit";
 function div_hide(){
 document.getElementById('login-container').style.display = "none";
 }
+
+function div_signup_show() {
+document.getElementById('signup-container').style.display = "inherit";
+}
+
+
+function div_signup_hide(){
+document.getElementById('signup-container').style.display = "none";
+}
