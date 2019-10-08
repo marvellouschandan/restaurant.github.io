b6e9a01c92184a7ce407bc8e1511d2e60a2fae66
Rajputakshay <akshaykumar772@yahoo.com>
2019-10-08 18:45:34 +0530

    optimizing login and signup page , adding testimonials section

--- script/login_container.js
+++ script/login_container.js
@@ -13,10 +13,39 @@ function div_signup_show() {
 }
 
 function div_signup_hide() {
+
 	document.getElementById('signup-container').style.display = "none";
 }
 
 function div_back_button_hide(){
 	div_signup_hide();
 	div_login_show();
+}
+ 
+function showPassword(){
+ 	var x = document.getElementById("password");
+
+ 	if(x.type ==="password")
+ 	{
+ 		x.type = "text";
+ 		document.getElementById("eye").setAttribute("class","fas fa-eye-slash");
+ 	}
+ 	else{
+ 	 	x.type = "password";
+ 	 	document.getElementById("eye").setAttribute("class","fas fa-eye");
+ 	}
+}
+
+function showmePassword(){
+ 	var x = document.getElementById("signUpPassword");
+
+ 	if(x.type ==="password")
+ 	{
+ 		x.type = "text";
+ 		document.getElementById("myeye").setAttribute("class","fas fa-eye-slash");
+ 	}
+ 	else{
+ 	 	x.type = "password";
+ 	 	document.getElementById("myeye").setAttribute("class","fas fa-eye");
+ 	}
 }
